32bd5c2d4c90ec5bec95f6caa4f8371d96a45489
Tara <t.mulcair1@nuigalway.ie>
2020-02-28 20:22:16 +0000

    my login page start work

---
+++ src/main/java/com/example/SwimApp/SwimAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.SwimApp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SwimApp/SwimAppApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.SwimApp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SwimAppApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
